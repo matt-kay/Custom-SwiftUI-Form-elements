1ab0d6ddac201ad99396c0d5d1438531ccbbd775
matt-kay <kayodematthew56@gmail.com>
2023-04-17 17:18:10 +0100

    Initial Commit

---
+++ CustomSwiftUiFormElements/CustomSwiftUiFormElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUiFormElementsApp.swift
+//  CustomSwiftUiFormElements
+//
+//  Created by Mattosha on 17/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUiFormElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
